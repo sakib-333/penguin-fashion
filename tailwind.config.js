db58ed3ef183e0928b5d90946ebf8e570a7e12f8
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-08-17 14:19:23 +0600

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+      bebasNneue: ["Bebas Neue", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
