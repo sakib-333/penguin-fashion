4a09a870ac9f2bf8f5198082f7e72a3290355682
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-08-17 16:20:15 +0600

    Add banner section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
     },
     colors: {
       "penguin-100": "#FFFBF0",
+      "penguin-200": "#A4BC46",
+      "penguin-300": "#85A019",
+      t1: "#FABE4C",
+      t2: "#363958",
+      t3: "#3E3E3E",
+      t4: "#ffffff",
     },
   },
   plugins: [],
